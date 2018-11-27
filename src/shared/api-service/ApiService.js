8c0adb5726cc7e52ed194c56c9135510c93bdc25
Netanel Draiman [ICG-IT] <nd54462@imceu.eu.ssmb.com>
2018-11-27 19:45:09 +0200

    Added Api methods for getPart and submitPart

--- src/shared/api-service/ApiService.js
+++ src/shared/api-service/ApiService.js
@@ -1,7 +1,20 @@
+import axios from 'axios';
+const baseUrl = 'http://18.191.23.1:5000';
+
+function getParts() {
+    return axios.get(`${baseUrl}/part`).then(result => result.data);
+}
+
+function submitPart(id, instrument, notes) {
+    return axios.post(`${baseUrl}/part/${id}`, { instrument, notes }).then(result => result.data);
+}
+
 function joinRoom(name, instrument) {
     return new Promise(resolve => setTimeout(() => resolve({ name, instrument }), 1000));
 }
 
 export default {
+    getParts,
+    submitPart,
     joinRoom
 };
