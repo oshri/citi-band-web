5cb980a7becd063c0e99ef5ae66d906359304a80
Netanel Draiman [ICG-IT] <nd54462@imceu.eu.ssmb.com>
2018-11-27 22:56:23 +0200

    Cleanup JoinFrom Component and moved some functionality to App component

--- src/App.js
+++ src/App.js
@@ -3,57 +3,116 @@ import './App.scss';
 import JoinForm from './components/JoinForm/JoinForm';
 import ApiService from './shared/api-service/ApiService';
 
+const userKey = 'user';
+
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            user: null,
-            otherUsers: {}
+            user: JSON.parse(localStorage.getItem(userKey)),
+            allUsers: {},
+            loading: true,
+            hasError: false
         };
+
+        console.log('Loaded user', this.state.user);
 
         ApiService.getParts()
             .then(parts => {
-                this.setState({ otherUsers: parts });
+                this.setState({ allUsers: parts, loading: false });
             })
-            .catch(error => console.log('failed to get other users', error));
+            .catch(error => {
+                console.log('failed to get other users', error);
+                this.setState({ loading: false, hasError: true });
+            });
+    }
+
+    getProgressView(label) {
+        const style = {
+            textAlign: 'center',
+            margin: '20px 0',
+            fontSize: '2rem'
+        };
+
+        return <div style={style}>{label}</div>;
     }
 
     onJoin(user) {
         console.log('onJoin', user);
-        this.setState({ user });
+        localStorage.setItem(userKey, JSON.stringify(user));
+        this.setState({ user, allUsers: { ...this.state.allUsers, [user.id]: user } });
+    }
+
+    getNewUserId() {
+        const ids = Object.keys(this.state.allUsers).map(id => parseInt(id, 10));
+
+        if (ids.length === 0) {
+            return 0;
+        }
+
+        return ids.sort()[ids.length - 1] + 1;
+    }
+
+    displayJoinForm() {
+        return <JoinForm onJoin={this.onJoin.bind(this)} userId={this.getNewUserId()} />;
+    }
+
+    resetAllParts() {
+        ApiService.reset()
+            .then(result => {
+                console.log('reset result', result);
+                this.setState({ user: null });
+            })
+            .catch(error => console.error('reset error', error));
     }
 
     displayUsers() {
-        const users = Object.keys(this.state.otherUsers)
-            .map(id => ({ id, ...this.state.otherUsers[id] }))
-            .concat([this.state.user]);
+        const users = Object.keys(this.state.allUsers).map(id => ({
+            id: parseInt(id, 10),
+            ...this.state.allUsers[id]
+        }));
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <td>Id</td>
-                        <td>Name</td>
-                        <td>Instrument</td>
-                        <td>Notes</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user, index) => (
-                        <tr key={index}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.instrument}</td>
-                            <td>{user.notes}</td>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Id</td>
+                            <td>Name</td>
+                            <td>Instrument</td>
+                            <td>Notes</td>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={index} className={user.id === this.state.user.id ? 'App__isUserRow' : ''}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.instrument}</td>
+                                <td>{user.notes}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className="App__reset-btn">
+                    <button type="button" onClick={this.resetAllParts.bind(this)}>
+                        Remove All Participants
+                    </button>
+                </div>
+            </div>
         );
     }
 
     render() {
-        return <div>{this.state.user ? this.displayUsers() : <JoinForm onJoin={this.onJoin.bind(this)} />}</div>;
+        if (this.state.loading) {
+            return this.getProgressView('Loading...');
+        }
+
+        if (this.state.hasError) {
+            return this.getProgressView('ERROR: Failed to load app');
+        }
+
+        return <div>{this.state.user ? this.displayUsers() : this.displayJoinForm()}</div>;
     }
 }
 
--- src/components/JoinForm/JoinForm.js
+++ src/components/JoinForm/JoinForm.js
@@ -10,34 +10,11 @@ class JoinForm extends Component {
             name: '',
             instrument: '',
             joining: false,
-            id: '',
-            gettingLastId: true,
-            failedLastId: false,
-            didMount: false
+            id: props.userId,
+            failedLastId: false
         };
 
-        this.getLastId();
-    }
-
-    componentDidMount() {
-        this.setState({ didMount: true });
-    }
-
-    getLastId() {
-        if (this.state.didMount) {
-            this.setState({ gettingLastId: true });
-        }
-
-        ApiService.getUserId()
-            .then(id => {
-                console.log('id', id);
-                this.setState({ id });
-            })
-            .catch(error => {
-                console.error('Failed to get last id', error);
-                this.setState({ failedLastId: true });
-            })
-            .finally(() => this.setState({ gettingLastId: false }));
+        console.log('JoinForm - user id', this.state.id);
     }
 
     handleName(event) {
@@ -59,6 +36,7 @@ class JoinForm extends Component {
         ApiService.submitPart(this.state.id, this.state.name, this.state.instrument, [])
             .then(result => {
                 console.log('result', result);
+                this.setState({ joining: false });
                 this.props.onJoin({ id: this.state.id, name: this.state.name, instrument: this.state.instrument });
             })
             .catch(error => {
@@ -100,15 +78,6 @@ class JoinForm extends Component {
         return <div style={style}>{label}</div>;
     }
 
-    resetAllParts() {
-        ApiService.reset()
-            .then(result => {
-                console.log('reset result', result);
-                return this.getLastId();
-            })
-            .catch(error => console.error('reset error', error));
-    }
-
     getJoinFormView() {
         return (
             <form className="JoinForm" onSubmit={this.handleSubmit.bind(this)}>
@@ -123,24 +92,11 @@ class JoinForm extends Component {
                     {this.getInstrumentButton('drums')}
                 </div>
                 <input type="submit" value="Join" />
-                <div className="JoinForm__reset-btn">
-                    <button type="button" onClick={this.resetAllParts.bind(this)}>
-                        Remove All Participants
-                    </button>
-                </div>
             </form>
         );
     }
 
     render() {
-        if (this.state.failedLastId) {
-            return this.getProgressView('ERROR: Failed to get last id');
-        }
-
-        if (this.state.gettingLastId) {
-            return this.getProgressView('Loading...');
-        }
-
         return this.state.joining ? this.getProgressView('Joining...') : this.getJoinFormView();
     }
 }
